cfe135ef54b218d1134acad381c97c99b78fdacc
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2010-01-02 17:38:21 +0100

    initial

---
+++ project.clj
@@ -0,0 +1,1 @@
+(defproject clarsec "1.0.0-SNAPSHOT" :description "FIXME: write" :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"] [org.clojure/clojure-contrib "1.0-SNAPSHOT"]] :main clarsec)
---
+++ src/clarsec.clj
@@ -0,0 +1,47 @@
+(ns clarsec
+  (:gen-class)
+  (:use [clojure.contrib.monads]))
+
+(defn consumed? [x]  (= (:type x) :consumed)) 
+(defn failed? [x]  (= (:type x) :failed)) 
+
+(defn failed [] {:type :failed})
+(defn consumed [value rest] {:type :consumed
+			     :value value
+			     :rest rest})
+
+(defmonad parser-m 
+  [m-result (fn m-result-parser [x] (fn [str] {:type :consumed
+			       :value x
+			       :rest str}))
+   m-bind (fn m-bind-parser [parser func]
+	    (fn [strn]
+	      (let [result (parser strn)]
+		(if (consumed? result)
+		  ((func (:value result)) (:rest result))
+		  result
+		))))
+
+   m-zero (fn [strn] (failed))
+   
+   m-plus (fn [& parsers] (println "ciao" parsers))
+   ]
+)
+
+(defmonadfn any-char [strn]
+      (if (= "" strn)
+          (failed)
+	  (consumed (first strn)
+		    (. strn (substring 1))))
+)
+
+
+(defmonadfn body [] 
+  (domonad [x any-char
+	    y any-char] (str y x))
+)
+
+
+(defn mytest [ n] 
+  (with-monad parser-m ((body) n)
+    ))
