086cc480530ad51a72518ae65963f166414df09e
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2010-03-29 04:48:45 +0200

    leningen clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject clarsec "0.0.1-SNAPSHOT" :description "Parsec ported to clojure" :dependencies [
         [org.clojure/clojure "1.1.0"]
         [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-        ] :main eu.dnetlib.dlms.parser)
+        ] 
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]]
+  :main eu.dnetlib.dlms.parser)
